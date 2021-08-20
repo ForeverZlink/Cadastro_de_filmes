ab384b540214acf649a031be4164f20881264d43
St;ZlinkG <carloscunha080@gmail.com>
2021-08-19 21:43:13 -0300

    Criei a view de home_page e o arquivo html

--- filmes/views.py
+++ filmes/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 
-def 
+def home_page(request):
+    return render(request,template_name='filmes/home_page.html')
