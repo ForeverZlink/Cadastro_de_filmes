e5061452bcbd87de0a72c29a9efca08fc93ae4b5
St;ZlinkG <carloscunha080@gmail.com>
2021-08-19 21:07:33 -0300

    Criei meu model para os filmes'

--- filmes/models.py
+++ filmes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Filme(models.Model):
+    name = models.CharField(
+        max_length=40, unique=True, blank=False, null=False
+        )
+    description = models.TextField()
+    avaliation = models.DecimalField(decimal_places=1,max_digits=1)
+    date_of_creation = models.DateTimeField(auto_now_add=True)
+    date_of_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
