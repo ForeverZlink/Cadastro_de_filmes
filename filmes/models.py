b1d9cefd1c06b167c9dd412d8c1283d3d00344e5
St;ZlinkG <carloscunha080@gmail.com>
2021-08-19 21:29:08 -0300

    Criei meu modulo de teste. Nele, criei um testcase para as queries relacionadas ao model Filme

--- filmes/models.py
+++ filmes/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
     
 class Filme(models.Model):
-    user = models.ForeignKey(User on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(
         max_length=40, unique=True, blank=False, null=False
         )
     description = models.TextField()
-    avaliation = models.DecimalField(decimal_places=1,max_digits=1)
+    avaliation = models.DecimalField(decimal_places=1,max_digits=2)
     date_of_creation = models.DateTimeField(auto_now_add=True)
     date_of_update = models.DateTimeField(auto_now=True)
 
--- filmes/tests.py
+++ filmes/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from filmes.models import Filme
+from datetime import datetime
 # Create your tests here.
+class TestQueries (TestCase):
+    @classmethod
+    def setUp(self):
+        self.filme = Filme
+        self.user = User.objects.create_user(username='carlos',password='123')
+
+    def test_criar_filme(self):
+        hora_date = datetime.now()
+        self.filme.objects.create(
+            user=self.user,name='ola',description='zen'
+            ,avaliation=3.1,date_of_creation=hora_date
+            )
+        
