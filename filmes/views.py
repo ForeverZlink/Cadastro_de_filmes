656f82bcbe8368820c86fc487ff32d1cb18ebc58
St;ZlinkG <carloscunha080@gmail.com>
2021-08-26 20:40:13 -0300

    Criei um sistema de login

--- filmes/views.py
+++ filmes/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render
 from django.views import View
 from filmes.models import Filme
 from django.shortcuts import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url="/account/login")
 def home_page(request):
     filmes = Filme.objects.all()
     return render(request,
     template_name='filmes/home_page.html', context ={'filmes':filmes}
     )
-
+@login_required(login_url='/account/login')
 def create_new_movie(request):
     from filmes.forms import FilmeForm
     from django.http import HttpResponseRedirect
@@ -19,6 +21,7 @@ def create_new_movie(request):
         
         if filme.is_valid():
             avaliation= float(request.POST['avaliation'])
+            #testa se a nota é maior 10 ou menor do que 0
             if  avaliation>10 or avaliation <0:
                 form_erros_avaliation = 'Digite um número entre 0 e 10'
                 return render(request,'filmes/new_movie.html'
--- site_de_filmes/urls.py
+++ site_de_filmes/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('filmes.urls'))
+    path('',include('filmes.urls')),
+    path('account/',include('django.contrib.auth.urls'))
 
 ]
