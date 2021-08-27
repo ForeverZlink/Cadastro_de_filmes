81ab8b81ac2d6555af431ff9ea8c55c3bcd875b4
St;ZlinkG <carloscunha080@gmail.com>
2021-08-26 22:37:46 -0300

    Criei um validador que verifica se o nome do filme já existe e adicionei comentários ao código

--- filmes/views.py
+++ filmes/views.py
@@ -17,19 +17,39 @@ def create_new_movie(request):
     from django.http import HttpResponseRedirect
 
     if request.method == 'POST':
+        #cria um instace do formulário
         filme=FilmeForm(request.POST)
         
         if filme.is_valid():
             avaliation= float(request.POST['avaliation'])
             #testa se a nota é maior 10 ou menor do que 0
-            if  avaliation>10 or avaliation <0:
+            if  (avaliation>10 or avaliation <0):
                 form_erros_avaliation = 'Digite um número entre 0 e 10'
                 return render(request,'filmes/new_movie.html'
                     ,context={'form_errors':form_erros_avaliation})
             else:
                 filme.save()
                 return HttpResponseRedirect(reverse('filmes:home_page'))
-        
+        else:
+            #os errors que podem ser gerados
+            some_errors = {'filme_ja_existe':
+                "Filme with this Name already exists."
+            }
+            #pega os errors 
+            error = filme.errors.as_json()
+            
+
+            #verifica se o motivo do erro é o filme existir
+            if some_errors['filme_ja_existe'] in error:
+
+                #recebe o erro
+                error = 'Já existe um filme com esse nome'
+           
+           
+            return render(request,
+            'filmes/new_movie.html',
+            context={'form_errors':error})
+
             
            
 
