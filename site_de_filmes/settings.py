f33fcd97b6a32d62ee391cf7320ef12cf520df16
St;ZlinkG <carloscunha080@gmail.com>
2021-09-02 19:49:37 -0300

    Resolvi o problema de deploy do heroku. O heroku não estava conseguindo coletar os aquivos estáticos pois a variável STATIFILES_DIRS estava apontando para para uma pasta static que não existia no base do projeto

--- site_de_filmes/settings.py
+++ site_de_filmes/settings.py
@@ -132,9 +132,9 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 LOGIN_REDIRECT_URL = '/home_page/'
 
